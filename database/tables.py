850598e7329418f47df9192e241ebdf047ab62bb
theMegaEXP <theMegaEXP74@gmail.com>
2024-05-19 15:18:13 -0700

    update tables.py

--- database/tables.py
+++ database/tables.py
@@ -2,12 +2,12 @@ from database.db import DB
 
 def create_tables():
     DB.Query.create_table('ingredients', ['id INTEGER PRIMARY KEY', 
-                                          'ingredient TEXT'])
+                                          'ingredient TEXT UNIQUE'])
     
     DB.Query.create_table('ingredient_times', ['id INTEGER PRIMARY KEY',
                                                'ingredient_id INTEGER',
-                                               'date TEXT',
-                                               'time TEXT',
+                                               'date TEXT NULL',
+                                               'time TEXT NULL',
                                                'datetime TEXT',
                                                'FOREIGN KEY (ingredient_id) REFERENCES ingredients(id)'])
 
@@ -22,13 +22,13 @@ def create_tables():
                                                   'PRIMARY KEY (product_id, ingredient_id)'])
 
     DB.Query.create_table('symptoms', ['id INTEGER PRIMARY KEY', 
-                                       'symptom TEXT'])
+                                       'symptom TEXT UNIQUE'])
 
     DB.Query.create_table('symptom_times', ['id INTEGER PRIMARY KEY', 
                                             'symptom_id INTEGER', 
                                             'severity INTEGER CHECK(severity >= 1 AND severity <= 10)',
-                                            'date TEXT',
-                                            'time TEXT',
+                                            'date TEXT NULL',
+                                            'time TEXT NULL',
                                             'datetime TEXT',
                                             'FOREIGN KEY (symptom_id) REFERENCES symptoms(id)'])
 
