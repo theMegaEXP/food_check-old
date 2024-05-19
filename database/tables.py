ab3390ad9d6ae9517d70f5627875cccc444b95f7
theMegaEXP <theMegaEXP74@gmail.com>
2024-05-19 14:53:22 -0700

    create ingredient_times table and update symptom_times table

--- database/tables.py
+++ database/tables.py
@@ -2,21 +2,33 @@ from database.db import DB
 
 def create_tables():
     DB.Query.create_table('ingredients', ['id INTEGER PRIMARY KEY', 
-                                   'ingredient TEXT'])
+                                          'ingredient TEXT'])
+    
+    DB.Query.create_table('ingredient_times', ['id INTEGER PRIMARY KEY',
+                                               'ingredient_id INTEGER',
+                                               'date TEXT',
+                                               'time TEXT',
+                                               'datetime TEXT',
+                                               'FOREIGN KEY (ingredient_id) REFERENCES ingredients(id)'])
 
     DB.Query.create_table('products', ['id INTEGER PRIMARY KEY', 
                                     'product TEXT', 
                                     'barcode TEXT'])
+    
+    DB.Query.create_table('product_ingredients', ['product_id INTEGER',
+                                                  'ingredient_id INTEGER',
+                                                  'FOREIGN KEY (product_id) REFERENCES products(id)',
+                                                  'FOREIGN KEY (ingredient_id) REFERENCES ingredients(id)',
+                                                  'PRIMARY KEY (product_id, ingredient_id)'])
 
-    DB.Query.create_table('symptoms_available', ['id INTEGER PRIMARY KEY', 
-                                            'symptom TEXT'])
+    DB.Query.create_table('symptoms', ['id INTEGER PRIMARY KEY', 
+                                       'symptom TEXT'])
 
     DB.Query.create_table('symptom_times', ['id INTEGER PRIMARY KEY', 
-                                        'symptom TEXT', 
-                                        'severity INTEGER CHECK(severity >= 1 AND severity <= 10)'])
+                                            'symptom_id INTEGER', 
+                                            'severity INTEGER CHECK(severity >= 1 AND severity <= 10)',
+                                            'date TEXT',
+                                            'time TEXT',
+                                            'datetime TEXT',
+                                            'FOREIGN KEY (symptom_id) REFERENCES symptoms(id)'])
 
-    DB.Query.create_table('product_ingredients', ['product_id INTEGER',
-                                            'ingredient_id INTEGER',
-                                            'FOREIGN KEY(product_id) REFERENCES products(id)',
-                                            'FOREIGN KEY(ingredient_id) REFERENCES ingredients(id)',
-                                            'PRIMARY KEY (product_id, ingredient_id)'])
