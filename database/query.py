c35a357122651bf30081e67332f7c9e783a2e53e
theMegaEXP <theMegaEXP74@gmail.com>
2024-05-19 12:58:03 -0700

    make Query class nested in DB class

--- database/query.py
+++ database/query.py
@@ -1,29 +1,35 @@
 from init import cursor as c, conn
 
-class Query:
-    def query_print(query: str):
-        c.execute(query)
-        print(c.fetchall())
-   
-    def create_table(table: str, columns: list[str]):
-        columns_str = ', '.join(columns)
+class DB:
+
+    class Query:
+        def query_print(query: str):
+            c.execute(query)
+            print(c.fetchall())
+    
+        def create_table(table: str, columns: list[str]):
+            columns_str = ', '.join(columns)
+            
+            c.execute(f"CREATE TABLE IF NOT EXISTS {table} ({columns_str});")
+            conn.commit()
+
+        def drop_table(table: str):
+            c.execute(f"DROP TABLE IF EXISTS {table}")
+            conn.commit()
+
+        def insert_into(table: str, columns: list, values: list):
+            columns_str = ", ".join(columns)
+            values_str = ", ".join([f"'{value}'" for value in values])
+
+            c.execute(f"INSERT INTO {table} ({columns_str}) VALUES ({values_str})")
+            conn.commit()
+
+        def fetch_table(table: str):
+            c.execute(f"SELECT * FROM {table};")
+            return c.fetchall()
+    
+    class View:
+        pass
         
-        c.execute(f"CREATE TABLE IF NOT EXISTS {table} ({columns_str});")
-        conn.commit()
-
-    def drop_table(table: str):
-        c.execute(f"DROP TABLE IF EXISTS {table}")
-        conn.commit()
-
-    def insert_into(table: str, columns: list, values: list):
-        columns_str = ", ".join(columns)
-        values_str = ", ".join([f"'{value}'" for value in values])
-
-        c.execute(f"INSERT INTO {table} ({columns_str}) VALUES ({values_str})")
-        conn.commit()
-
-    def fetch_table(table: str):
-        c.execute(f"SELECT * FROM {table};")
-        return c.fetchall()
 
     
--- database/tables.py
+++ database/tables.py
@@ -1,9 +1,9 @@
-from query import Query
+from query import DB
 
 from init import cursor as c, conn
 
 def show_tables():
-    Query.query_print("SELECT name FROM sqlite_master WHERE type='table';")
+    DB.Query.query_print("SELECT name FROM sqlite_master WHERE type='table';")
 
 def delete_tables():
     c.execute("SELECT name FROM sqlite_master WHERE type='table';")
@@ -11,27 +11,27 @@ def delete_tables():
 
     for table in tables:
         table_name = table[0]
-        Query.drop_table(table_name)
+        DB.Query.drop_table(table_name)
         print(f"{table_name} deleted.")
         
 
 
 def create_tables():
-    Query.create_table('ingredients', ['id INTEGER PRIMARY KEY', 
+    DB.Query.create_table('ingredients', ['id INTEGER PRIMARY KEY', 
                                    'ingredient TEXT'])
 
-    Query.create_table('products', ['id INTEGER PRIMARY KEY', 
+    DB.Query.create_table('products', ['id INTEGER PRIMARY KEY', 
                                     'product TEXT', 
                                     'barcode TEXT'])
 
-    Query.create_table('symptoms_available', ['id INTEGER PRIMARY KEY', 
+    DB.Query.create_table('symptoms_available', ['id INTEGER PRIMARY KEY', 
                                             'symptom TEXT'])
 
-    Query.create_table('symptom_times', ['id INTEGER PRIMARY KEY', 
+    DB.Query.create_table('symptom_times', ['id INTEGER PRIMARY KEY', 
                                         'symptom TEXT', 
                                         'severity INTEGER CHECK(severity >= 1 AND severity <= 10)'])
 
-    Query.create_table('product_ingredients', ['product_id INTEGER',
+    DB.Query.create_table('product_ingredients', ['product_id INTEGER',
                                             'ingredient_id INTEGER',
                                             'FOREIGN KEY(product_id) REFERENCES products(id)',
                                             'FOREIGN KEY(ingredient_id) REFERENCES ingredients(id)',
@@ -39,8 +39,8 @@ def create_tables():
     
 create_tables()
 show_tables()
-Query.insert_into('products', ['product', 'barcode'], ['Apple', 'N/A'])
-print(Query.fetch_table('products'))
+DB.Query.insert_into('products', ['product', 'barcode'], ['Apple', 'N/A'])
+print(DB.Query.fetch_table('products'))
 print()
 
 delete_tables()
