8ea50a73667c3f5a3cda5b7a9ff7917e6a560933
theMegaEXP <theMegaEXP74@gmail.com>
2024-05-20 00:27:36 -0700

    display options to user for database in commandline and prevent errors

--- commandline/database.py
+++ commandline/database.py
@@ -4,10 +4,15 @@ from database.init import cursor as c, conn
 from database.db import DB
 from commandline.print import Print
 from database.import_sql import data_to_db
+import database.tables as tables
 
 def run_db_query():
     Print.bold("You have entered the place to access the database.")
     print("Type 'exit' to exit. Type 'show' followed by a table to show a table.")
+    print("Type 'reset' to reset the database.")
+    print("Type 'show' followed by a table name to view a DB table.")
+    print("Yype 'start' to start inserting data into the database.")
+    print("Type -q followed by a query to query the database. WARNING: If you edit any tables, you may need to restart the app.")
     
     while True:
         query_input = input()
@@ -17,6 +22,7 @@ def run_db_query():
 
         elif query_input == 'reset':
             DB.Operations.reset()
+            tables.create_tables()
 
         elif query_input == 'start':
             data_to_db()
@@ -26,14 +32,18 @@ def run_db_query():
             
             try:
                 DB.View.table(table_name)
-            except ValueError:
+            except Exception:
                 Print.red(f"Table {table_name} does not exist.")
            
-        else:
+        elif re.search(r'^-q .*', query_input):
             try:
-                c.execute(query_input)
+                query = query_input[3::]
+                c.execute(query)
                 print(c.fetchall())
-            except ValueError:
-                print(ValueError)
+            except Exception:
+                Print.red("This query raised an error.")
+
+        else:
+            Print.red("Your value is not valid.")
 
         
