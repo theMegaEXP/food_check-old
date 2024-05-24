0df00e042c5b215b803f5d86ffb4d499939f3a49
theMegaEXP <theMegaEXP74@gmail.com>
2024-05-24 13:57:06 -0700

    add hour_to_unit and calc_time_diff methods to helpers.py

--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import re
+import math
 
 def format_datetime(date_str, time_str):
     date = datetime.strptime(date_str, '%m-%d-%Y')
@@ -12,3 +13,21 @@ def format_datetime(date_str, time_str):
 
 def ingredients_to_array(ingredients):
     return [elem for elem in re.split(r', |\(|\)', ingredients) if elem != '']
+
+def hour_to_unit(hour):
+    if hour > 1 and isinstance(hour, float):
+        decimal, integer = math.modf(hour)
+        return f"{int(integer)}h {int(decimal * 60)}m"
+    elif hour >= 1:
+        return f"{hour}h"
+    else:
+        return f"{int(hour * 60)}m"
+    
+def calc_time_diff(date1, date2):
+    datetime1 = datetime.strptime(date1, '%Y-%m-%d %H:%M:%S')
+    datetime2 = datetime.strptime(date2, '%Y-%m-%d %H:%M:%S')
+
+    time_diff = abs(datetime1 - datetime2)
+    hours_diff = time_diff.total_seconds() / 3600
+
+    return hours_diff
