08ae0e2312de1688aa53abe93b00fbbbb8ae2a26
theMegaEXP <theMegaEXP74@gmail.com>
2024-05-24 13:45:28 -0700

    add Reults class to calculate.py and move code from calculate() to likely_symptom_cuase()

--- calculate.py
+++ calculate.py
@@ -1,69 +1,72 @@
 from database.db import DB
 from commandline.print import Print
 
-def calculate():
-    # query = """
-    #     SELECT
-    #         symptoms.symptom,
-    #         symptom_times.severity,
-    #         (julianday(symptom_times.datetime) - julianday(ingredient_times.datetime)) * 24 AS symptom_delay,
-    #         ingredients.ingredient
-    #     FROM
-    #         symptoms
-    #     JOIN
-    #         symptom_times ON symptoms.id = symptom_times.symptom_id
-    #     JOIN
-    #         ingredient_times ON ingredient_times.id = symptom_times.symptom_id
-    #     JOIN
-    #         ingredients ON ingredients.id = ingredient_times.ingredient_id
-    #     WHERE
-    #         symptom_delay > 0 AND symptom_delay < 24
-    #     GROUP BY 
-    #         ingredients.id
-    #     ORDER BY
-    #         COUNT(*) * AVG(symptom_times.severity) DESC
-    #     LIMIT
-    #         1;
+class Results:
+
+    def calculate():
+        pass
+        # query = """
+        #     SELECT
+        #         symptoms.symptom,
+        #         symptom_times.severity,
+        #         (julianday(symptom_times.datetime) - julianday(ingredient_times.datetime)) * 24 AS symptom_delay,
+        #         ingredients.ingredient
+        #     FROM
+        #         symptoms
+        #     JOIN
+        #         symptom_times ON symptoms.id = symptom_times.symptom_id
+        #     JOIN
+        #         ingredient_times ON ingredient_times.id = symptom_times.symptom_id
+        #     JOIN
+        #         ingredients ON ingredients.id = ingredient_times.ingredient_id
+        #     WHERE
+        #         symptom_delay > 0 AND symptom_delay < 24
+        #     GROUP BY 
+        #         ingredients.id
+        #     ORDER BY
+        #         COUNT(*) * AVG(symptom_times.severity) DESC
+        #     LIMIT
+        #         1;
+            
+        # """
+
+    def likey_symptom_cause():
+        query = """
+            SELECT
+                symptoms.symptom,
+                (
+                    SELECT
+                        ingredients.ingredient
+                    FROM
+                        symptoms AS s
+                    JOIN
+                        symptom_times AS st ON s.id = st.symptom_id
+                    JOIN
+                        ingredient_times AS it ON it.id = st.symptom_id
+                    JOIN
+                        ingredients ON ingredients.id = it.ingredient_id
+                    WHERE
+                        s.id = symptoms.id 
+                        AND 
+                        (julianday(st.datetime) - julianday(it.datetime)) * 24 BETWEEN 0 AND 24 
+                        AND 
+                        ingredients.ingredient NOT IN (SELECT ingredient FROM ignored_ingredients)
+                    GROUP BY
+                        ingredients.id
+                    ORDER BY
+                        COUNT(*) * AVG(st.severity) DESC
+                    LIMIT
+                        1
+                ) AS top_ingredient
+            FROM
+                symptoms;
+        """
+        results = DB.Query.query_results(query)
         
-    # """
+        Print.underline_bold("Most likely ingredient to be causing each symptom.")
+        for result in results:
+            symptom = result[0].title()
+            ingredient = result[1] if result[1] != None else 'N/A'
 
-    query = """
-        SELECT
-            symptoms.symptom,
-            (
-                SELECT
-                    ingredients.ingredient
-                FROM
-                    symptoms AS s
-                JOIN
-                    symptom_times AS st ON s.id = st.symptom_id
-                JOIN
-                    ingredient_times AS it ON it.id = st.symptom_id
-                JOIN
-                    ingredients ON ingredients.id = it.ingredient_id
-                WHERE
-                    s.id = symptoms.id 
-                    AND 
-                    (julianday(st.datetime) - julianday(it.datetime)) * 24 BETWEEN 0 AND 24 
-                    AND 
-                    ingredients.ingredient NOT IN (SELECT ingredient FROM ignored_ingredients)
-                GROUP BY
-                    ingredients.id
-                ORDER BY
-                    COUNT(*) * AVG(st.severity) DESC
-                LIMIT
-                    1
-            ) AS top_ingredient
-        FROM
-            symptoms;
-    """
-    results = DB.Query.query_results(query)
-    print(results)
-    
-    Print.underline_bold("Most likely ingredient to be causing each symptom.")
-    for result in results:
-        symptom = result[0].title()
-        ingredient = result[1] if result[1] != None else 'N/A'
-
-        Print.key_value(symptom, ingredient)
+            Print.key_value(symptom, ingredient)
 
